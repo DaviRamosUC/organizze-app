9f044836dc144330ab46f930c8c09c94ad899b45
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-02-21 15:40:48 -0300

    Modificando layout do slider de introdução e cadastro

--- app/src/main/java/com/devdavi/organizze/activity/MainActivity.java
+++ app/src/main/java/com/devdavi/organizze/activity/MainActivity.java
@@ -1,9 +1,13 @@
-package com.devdavi.organizze;
+package com.devdavi.organizze.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.devdavi.organizze.R;
 import com.devdavi.organizze.databinding.ActivityMainBinding;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
@@ -13,6 +17,7 @@ public class MainActivity extends IntroActivity {
 
     private ActivityMainBinding binding;
     private FirebaseAnalytics mFirebaseAnalytics;
+    private Button cadastrese;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,10 +51,26 @@ public class MainActivity extends IntroActivity {
                 new FragmentSlide.Builder()
                         .background(R.color.white)
                         .fragment(R.layout.intro_4)
+                        .build()
+        );
+
+        addSlide(
+                new FragmentSlide.Builder()
+                        .background(R.color.white)
+                        .fragment(R.layout.intro_cadastro)
                         .canGoForward(false)
                         .build()
         );
 
 
     }
+
+    public void btCadastrar(View view) {
+        startActivity(new Intent(this, CadastroActivity.class));
+    }
+
+    public void btEntrar(View view) {
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
 }
