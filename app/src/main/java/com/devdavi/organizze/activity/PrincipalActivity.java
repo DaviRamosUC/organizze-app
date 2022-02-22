46ad464200b9f811cf7c564b28bfec191cd923dd
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-02-22 13:48:58 -0300

    Adicionando lib https://github.com/Clans/FloatingActionButton e configurando fabs

--- app/src/main/java/com/devdavi/organizze/activity/PrincipalActivity.java
+++ app/src/main/java/com/devdavi/organizze/activity/PrincipalActivity.java
@@ -1,6 +1,8 @@
 package com.devdavi.organizze.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -11,33 +13,27 @@ import androidx.navigation.ui.NavigationUI;
 import com.devdavi.organizze.R;
 import com.devdavi.organizze.databinding.ActivityPrincipalBinding;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PrincipalActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
     private ActivityPrincipalBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityPrincipalBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_principal);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_principal);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    private void adicionarReceita(View view){
+
     }
+
+    private void adicionarDespesa(View view){
+
+    }
+
 }
