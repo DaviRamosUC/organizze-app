db5a70f1d23725dde047e1ad59f48543cbfd4cdf
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-02-23 11:25:13 -0300

    Gerando e salvando movimentação de despesa

--- app/src/main/java/com/devdavi/organizze/activity/DespesasActivity.java
+++ app/src/main/java/com/devdavi/organizze/activity/DespesasActivity.java
@@ -3,14 +3,26 @@ package com.devdavi.organizze.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.devdavi.organizze.R;
+import com.devdavi.organizze.config.ConfiguracaoFirebase;
 import com.devdavi.organizze.databinding.ActivityDespesasBinding;
+import com.devdavi.organizze.model.Movimentacao;
+import com.devdavi.organizze.utils.Base64Custom;
 import com.devdavi.organizze.utils.DateCustom;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.Objects;
 
 public class DespesasActivity extends AppCompatActivity {
 
     private ActivityDespesasBinding binding;
+    private Movimentacao movimentacao;
+    private String editData, editCategoria, editDescricao;
+    private Double valor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +34,19 @@ public class DespesasActivity extends AppCompatActivity {
         binding.editData.setText(DateCustom.dataAtual());
 
         binding.fab.setOnClickListener(view -> {
-
+            String quantia = binding.campoValor.getText().toString();
+            valor = quantia.isEmpty() ? 0 : Double.parseDouble(quantia);
+            if (valor == 0) {
+                Toast.makeText(getApplicationContext(), "Primeiro informe o valor", Toast.LENGTH_LONG)
+                        .show();
+            } else {
+                editData = Objects.requireNonNull(binding.editData.getText()).toString();
+                editCategoria = Objects.requireNonNull(binding.editCategoria.getText()).toString();
+                editDescricao = Objects.requireNonNull(binding.editDescricao.getText()).toString();
+                movimentacao = new Movimentacao(editData, editCategoria, editDescricao, "d", valor);
+                movimentacao.salvar();
+                finish();
+            }
         });
     }
 }
---
+++ app/src/main/java/com/devdavi/organizze/model/Movimentacao.java
@@ -0,0 +1,79 @@
+package com.devdavi.organizze.model;
+
+import com.devdavi.organizze.config.ConfiguracaoFirebase;
+import com.devdavi.organizze.utils.Base64Custom;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.Objects;
+
+public class Movimentacao {
+
+    private String data;
+    private String categoria;
+    private String descricao;
+    private String tipo;
+    private Double valor;
+
+    public Movimentacao() {
+    }
+
+    public Movimentacao(String data, String categoria, String descricao, String tipo, Double valor) {
+        this.data = data;
+        this.categoria = categoria;
+        this.descricao = descricao;
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public void salvar() {
+        FirebaseAuth auth = ConfiguracaoFirebase.getAutenticacao();
+        DatabaseReference database = ConfiguracaoFirebase.getDatabase();
+        String email = Base64Custom.codificarBase64(Objects.requireNonNull(Objects.requireNonNull(auth.getCurrentUser()).getEmail()));
+        database.child("movimentacao")
+                .child(email)
+                .child(this.data.replace("/", "").substring(2,data.length()-2))
+                .push()
+                .setValue(this);
+    }
+}
